e51f047d5342a5a3e00c979a0f2d6d6a42635634
Elton Ribeiro <ear.elton@gmail.com>
2022-05-23 22:00:56 -0300

    Update CustomerReview.java

--- src/main/java/com/ecommerce/model/CustomerReview.java
+++ src/main/java/com/ecommerce/model/CustomerReview.java
@@ -1,5 +1,7 @@
 package com.ecommerce.model;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,18 +13,110 @@ public class CustomerReview {
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private long id;
-	@Column (name = "REF_PEDIDO")
-	private String pedido;
-	@Column (name = "PRODUTO")
-	private String produto;
-	@Column (name = "TAMANHO_PRODUTO")
-	private String tamanho;
-	@Column (name = "QTD")
-	private String qtd;
-	@Column (name = "VALOR_TOTAL")
-	private String valorTotal;
-		
+	@Column (name = "NUM_REFERENCIA")
+	private int referenceNumber;
+	@Column (name = "MODELAGEM")
+	private String modeling;
+	@Column (name = "CINTURA")
+	private double waist;
+	@Column (name = "COMPRIMENTO")
+	private double length;
+	@Column (name = "ELASTICIDADE")
+	private String elasticity;
 	
-	
+	public CustomerReview(long id, int referenceNumber, String modeling, double waist, double length,
+			String elasticity) {
+		super();
+		this.id = id;
+		this.referenceNumber = referenceNumber;
+		this.modeling = modeling;
+		this.waist = waist;
+		this.length = length;
+		this.elasticity = elasticity;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public int getReferenceNumber() {
+		return referenceNumber;
+	}
+
+	public void setReferenceNumber(int referenceNumber) {
+		this.referenceNumber = referenceNumber;
+	}
+
+	public String getModeling() {
+		return modeling;
+	}
+
+	public void setModeling(String modeling) {
+		this.modeling = modeling;
+	}
+
+	public double getWaist() {
+		return waist;
+	}
+
+	public void setWaist(double waist) {
+		this.waist = waist;
+	}
+
+	public double getLength() {
+		return length;
+	}
+
+	public void setLength(double length) {
+		this.length = length;
+	}
+
+	public String getElasticity() {
+		return elasticity;
+	}
+
+	public void setElasticity(String elasticity) {
+		this.elasticity = elasticity;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(elasticity, id, length, modeling, referenceNumber, waist);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CustomerReview other = (CustomerReview) obj;
+		return Objects.equals(elasticity, other.elasticity) && id == other.id
+				&& Double.doubleToLongBits(length) == Double.doubleToLongBits(other.length)
+				&& Objects.equals(modeling, other.modeling) && referenceNumber == other.referenceNumber
+				&& Double.doubleToLongBits(waist) == Double.doubleToLongBits(other.waist);
+	}
+
+	@Override
+	public String toString() {
+		return "CustomerReview [id=" 
+				+ id 
+				+ ", referenceNumber=" 
+				+ referenceNumber 
+				+ ", modeling=" 
+				+ modeling
+				+ ", waist=" 
+				+ waist + ", length=" 
+				+ length 
+				+ ", elasticity=" 
+				+ elasticity 
+				+ "]";
+	}
 	
 }
